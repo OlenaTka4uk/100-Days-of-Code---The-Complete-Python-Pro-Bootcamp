f179eacab847669bc024260c8e0ed2f443b81d96
Olena Tkachuk <bushueva07@gmail.com>
2025-07-27 11:50:08 +0300

    Added new functionality

--- 5_Hangman_Game/hangman.py
+++ 5_Hangman_Game/hangman.py
@@ -1,6 +1,11 @@
 import random
+from hangman_art import stages
+from hangman_art import logo
+from word_list import word_list
 
-word_list =["wolf", "cat", "video"]
+lives = 6
+print(logo)
+print("Welcome to Hangman Game!")
 
 choosen_word = random.choice(word_list)
 
@@ -13,14 +18,40 @@ for position in range(len(choosen_word)):
 
 print(placeholder)
 
-guess = input("Guess a letter: ").lower()
+game_over = False
+correct_letters = []
 
-display = ""
+while not game_over:
+    guess = input("Guess a letter: ").lower()
 
-for letter in choosen_word:
-    if letter == guess:
-        display += letter
-    else:
-        display += "_"
+    if guess in correct_letters:
+        print("You already guessed that letter. Try again.")
+        continue
 
-print(display)
+    display = ""
+
+    for letter in choosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(letter)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print(display)
+
+    if guess not in choosen_word:
+        lives -= 1
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        if lives == 0:
+            game_over = True
+            print("You lose!")
+            print(f"The word was: {choosen_word}")
+
+    if "_" not in display:
+        game_over = True
+        print("You win!")
+
+    print(stages[lives])
+
