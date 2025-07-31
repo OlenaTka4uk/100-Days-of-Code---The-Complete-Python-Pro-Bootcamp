3605ea9ef3eb99ca6a7fae6c6a20013e4f96d8f9
Olena Tkachuk <bushueva07@gmail.com>
2025-07-31 16:32:17 +0300

    Added new users and dictionary

--- 7_Blind_Auction/blind_auction.py
+++ 7_Blind_Auction/blind_auction.py
@@ -3,17 +3,28 @@ import logo
 
 print(logo.logo)
 
-computer_bid = random.randint(50, 1001)
+number_of_users = random.randint(2, 10)
+users = {}
+
+while number_of_users > 0:
+    name = random.choice(['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Frank', 'Grace', 'Heidi', 'Ivan', 'Judy'])
+    bid = random.randint(50, 1000)
+    users[name] = bid
+    number_of_users -= 1
 
 user_name = input("What is your name? ")
 user_bid = int(input("What is your bid (from $50 till $1000)? $"))
+users[user_name] = user_bid
 
-def find_highest_bidder():
-    if user_bid > computer_bid:
-        print(f"{user_name} is winner with a bid of {user_bid}. Another bid was {computer_bid}")
-    elif user_bid == computer_bid:
-        print("The same bid")
-    else:
-        print(f"Sorry, but we have another winner with the bid of {computer_bid}!")
+def find_highest_bidder(users):
+    highest_bid = 0
+    winner = ""
 
-find_highest_bidder()
+    for user in users:
+        if users[user] > highest_bid:
+            highest_bid = users[user]
+            winner = user
+
+    print(f"The winner is {winner} with a bid of ${highest_bid}.")
+
+find_highest_bidder(users)
